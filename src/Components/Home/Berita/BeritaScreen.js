ae3dcaaeed72b02b6e396a6aa8cbff353e01ddc1
Lukman Yudokusumo <lukmanyudokusumo@umannnn.local>
2023-10-09 22:53:06 +0700

    home

--- src/Components/Home/Berita/BeritaScreen.js
+++ src/Components/Home/Berita/BeritaScreen.js
@@ -120,7 +120,7 @@ const BeritaScreen = () => {
 
   return (
     <div className="berita-container-full">
-      <h1 style={{ marginTop: "200px" }}>Berita Terbaru</h1>
+      <h1 style={{ marginTop: "100px" }}>Berita Terbaru</h1>
       <div className="berita-card-container">
         {beritaChunks.map((berita) => (
           <Link to={`/berita`} key={berita.id} className="berita-link">
