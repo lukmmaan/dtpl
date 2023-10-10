062913611b3d162471dabcd21d9b55d78ee0cd5d
Lukman Yudokusumo <lukmanyudokusumo@umannnn.local>
2023-10-10 22:29:41 +0700

    home

--- src/Components/ApprovalSurat/ApprovalSurat.js
+++ src/Components/ApprovalSurat/ApprovalSurat.js
@@ -5,6 +5,7 @@ import swal from "sweetalert";
 
 import "./ApprovalSurat.css";
 import { API_URL } from "../../Constants/Api";
+import { Surat } from "../../Assets/Surat";
 
 const ApprovalSurat = ({ user }) => {
   const [suratList, setSuratList] = useState([]);
@@ -103,6 +104,7 @@ const ApprovalSurat = ({ user }) => {
             <th>Jenis Surat</th>
             <th>Alamat</th>
             <th>Keperluan</th>
+            <th>Download Dokumen</th>
             <th>Aksi</th>
           </tr>
         </thead>
@@ -114,6 +116,34 @@ const ApprovalSurat = ({ user }) => {
               <td>{surat.jenisSurat}</td>
               <td>{surat.alamat}</td>
               <td>{surat.keperluan}</td>
+              <td>
+                <p>
+                  <a
+                    href={Surat} // Ganti dengan URL atau path yang sesuai dengan dokumen yang ingin Anda unduh
+                    download="Surat.pdf" // Nama file yang akan diunduh
+                    target="_blank"
+                    style={{
+                      marginLeft: "5px",
+                      backgroundColor: "#8B4513",
+                      color: "#fff",
+                      padding: "5px 10px",
+                      borderRadius: "5px",
+                      textDecoration: "none",
+                      transition: "background-color 0.3s ease-in-out",
+                    }}
+                    // Menambahkan gaya hover
+                    onMouseOver={(e) =>
+                      (e.target.style.backgroundColor = "chocolate")
+                    }
+                    onMouseOut={(e) =>
+                      (e.target.style.backgroundColor = "#8B4513")
+                    }
+                  >
+                    disini
+                  </a>
+                </p>
+              </td>
+
               <td>
                 <button
                   className="approve-button"
