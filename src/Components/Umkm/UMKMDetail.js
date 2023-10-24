7a36c5889d7dc9fdde63f9e9e0b20b2eb0c18699
Lukman Yudokusumo <lukmanyudokusumo@umannnn.local>
2023-10-24 23:19:34 +0700

    add dependencies

--- src/Components/Umkm/UMKMDetail.js
+++ src/Components/Umkm/UMKMDetail.js
@@ -9,7 +9,7 @@ import Axios from "axios";
 import { API_URL } from "../../Constants/Api";
 import Swal from "sweetalert2";
 
-const UMKMDetail = () => {
+const UMKMDetail = ({}) => {
   const [umkm, setUmkm] = useState([]);
   const { id } = useParams();
   const navigate = useNavigate();
@@ -142,30 +142,32 @@ const UMKMDetail = () => {
           </tr>
         </tbody>
       </table>
-      <button
-        className="decline-button"
-        onClick={() =>
-          Swal.fire({
-            title: "Yakin ingin delete umkm ini?",
-            text: "Anda akan menghapus data umkm ini",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "green",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Ya, Selesai",
-            cancelButtonText: "Batal",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire({
-                icon: "success",
-                title: "UMKM telah dihapus",
-                text: "Terima kasih atas kontribusi Anda!",
-              });
-              navigate("/umkm-list");
-            }
-          })
-        }
-      ></button>
+      {user.role === "kepalaDesa" && (
+        <button
+          className="decline-button"
+          onClick={() =>
+            Swal.fire({
+              title: "Yakin ingin delete umkm ini?",
+              text: "Anda akan menghapus data umkm ini",
+              icon: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "green",
+              cancelButtonColor: "#d33",
+              confirmButtonText: "Ya, Selesai",
+              cancelButtonText: "Batal",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire({
+                  icon: "success",
+                  title: "UMKM telah dihapus",
+                  text: "Terima kasih atas kontribusi Anda!",
+                });
+                navigate("/umkm-list");
+              }
+            })
+          }
+        ></button>
+      )}
     </div>
   ) : (
     <div className="umkm-detail-container">UMKM tidak ditemukan</div>
