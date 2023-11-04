1aeaddb633ff77102c7891f53fe7199db0dacdfa
Lukman Yudokusumo <lukmanyudokusumo@umannnn.local>
2023-11-05 04:09:22 +0700

    pengaduan

--- src/Components/DesaPengaduanList/DesaPengaduanList.js
+++ src/Components/DesaPengaduanList/DesaPengaduanList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import "./DesaPengaduanList.css"; // Impor file CSS
 import Swal from "sweetalert2";
 
@@ -8,27 +8,123 @@ class DesaPengaduanList extends Component {
     this.state = {
       pengaduanList: [
         {
-          nama: "Nama Pengaduan 1",
-          email: "pengadu1@mail.com",
-          alamat: "Alamat 1",
-          pengaduan: "Deskripsi pengaduan 1",
+          nama: "Bapak Yanto",
+          email: "yanto@mail.com",
+          alamat: "Jl. Raya Desa 1",
+          pengaduan: "Pohon tumbang di jalan desa",
           nomorTelepon: "081234567890",
           tanggalKejadian: "01-10-2023",
-          lokasiKejadian: "Lokasi 1",
+          lokasiKejadian: "Jalan Desa 1",
           prioritas: "Tinggi",
-          kategori: "Kategori 1",
+          kategori: "Kecelakaan Lalu Lintas",
           status: "pengaduan",
         },
         {
-          nama: "Nama Pengaduan 2",
-          email: "pengadu2@mail.com",
-          alamat: "Alamat 2",
-          pengaduan: "Deskripsi pengaduan 2",
+          nama: "Ibu Siti",
+          email: "siti@mail.com",
+          alamat: "Jl. Harapan 3",
+          pengaduan: "Banjir di rumah warga",
           nomorTelepon: "081234567891",
           tanggalKejadian: "02-10-2023",
-          lokasiKejadian: "Lokasi 2",
+          lokasiKejadian: "Rumah Warga 2",
           prioritas: "Rendah",
-          kategori: "Kategori 2",
+          kategori: "Bencana Alam",
+          status: "done",
+        },
+        {
+          nama: "Bapak Joko",
+          email: "joko@mail.com",
+          alamat: "Jl. Maju Sejahtera 7",
+          pengaduan: "Sarana olahraga rusak di lapangan desa",
+          nomorTelepon: "081234567892",
+          tanggalKejadian: "03-10-2023",
+          lokasiKejadian: "Lapangan Desa 3",
+          prioritas: "Sedang",
+          kategori: "Infrastruktur",
+          status: "pengaduan",
+        },
+        {
+          nama: "Ibu Rini",
+          email: "rini@mail.com",
+          alamat: "Jl. Cemerlang 2",
+          pengaduan: "Pelayanan kesehatan kurang memadai",
+          nomorTelepon: "081234567893",
+          tanggalKejadian: "04-10-2023",
+          lokasiKejadian: "Puskesmas 4",
+          prioritas: "Tinggi",
+          kategori: "Kesehatan",
+          status: "pengaduan",
+        },
+        {
+          nama: "Bapak Slamet",
+          email: "slamet@mail.com",
+          alamat: "Jl. Damai Sentosa 5",
+          pengaduan: "Rumah warga terbakar",
+          nomorTelepon: "081234567894",
+          tanggalKejadian: "05-10-2023",
+          lokasiKejadian: "Rumah Warga 5",
+          prioritas: "Rendah",
+          kategori: "Bencana Alam",
+          status: "done",
+        },
+        {
+          nama: "Ibu Ria",
+          email: "ria@mail.com",
+          alamat: "Jl. Bahagia 12",
+          pengaduan: "Sungai desa meluap",
+          nomorTelepon: "081234567895",
+          tanggalKejadian: "06-10-2023",
+          lokasiKejadian: "Sungai Desa 6",
+          prioritas: "Tinggi",
+          kategori: "Bencana Alam",
+          status: "pengaduan",
+        },
+        {
+          nama: "Bapak Supri",
+          email: "supri@mail.com",
+          alamat: "Jl. Maju Jaya 8",
+          pengaduan: "Lampu jalan mati",
+          nomorTelepon: "081234567896",
+          tanggalKejadian: "07-10-2023",
+          lokasiKejadian: "Jalan Desa 7",
+          prioritas: "Sedang",
+          kategori: "Infrastruktur",
+          status: "pengaduan",
+        },
+        {
+          nama: "Ibu Dini",
+          email: "dini@mail.com",
+          alamat: "Jl. Damai Indah 15",
+          pengaduan: "Kerusakan jembatan",
+          nomorTelepon: "081234567897",
+          tanggalKejadian: "08-10-2023",
+          lokasiKejadian: "Jembatan 8",
+          prioritas: "Tinggi",
+          kategori: "Infrastruktur",
+          status: "done",
+        },
+        {
+          nama: "Bapak Ali",
+          email: "ali@mail.com",
+          alamat: "Jl. Harapan Baru 21",
+          pengaduan: "Kebocoran pipa air",
+          nomorTelepon: "081234567898",
+          tanggalKejadian: "09-10-2023",
+          lokasiKejadian: "Pipa Air 9",
+          prioritas: "Sedang",
+          kategori: "Infrastruktur",
+          status: "pengaduan",
+        },
+        {
+          nama: "Ibu Jumiati",
+          email: "jumiati@mail.com",
+          alamat: "Jl. Sejahtera Makmur 30",
+          pengaduan: "Rumah tidak layak huni",
+          nomorTelepon: "081234567899",
+          tanggalKejadian: "10-10-2023",
+          lokasiKejadian: "Rumah 10",
+          prioritas: "Tinggi",
+          kategori: "Perumahan",
           status: "done",
         },
         // Tambahkan data pengaduan lainnya di sini
@@ -73,15 +169,20 @@ class DesaPengaduanList extends Component {
                       <button
                         style={{
                           backgroundColor: "chocolate",
-                          width: "80px",
-                          height: "30px",
+                          width: "100px", // Lebar tombol diperbesar
+                          height: "40px", // Tinggi tombol diperbesar
                           color: "white",
                           borderRadius: "8px",
+                          border: "none", // Hapus border
+                          cursor: "pointer", // Ganti kursor ketika diarahkan ke tombol
+                          fontSize: "16px", // Ukuran font diperbesar
+                          fontWeight: "bold", // Teks tebal
+                          transition: "background-color 0.3s", // Animasi perubahan latar belakang
                         }}
                         onClick={() => {
                           Swal.fire({
                             title: "Yakin ingin selesaikan pengaduan ini?",
-                            text: "Anda akan menyelesaikan pengaduan ini.",
+                            text: "Anda akan mengirimkan email kepada pembuat pengaduan ini, dan akan memproses pengaduan ini.",
                             icon: "warning",
                             showCancelButton: true,
                             confirmButtonColor: "green",
@@ -90,6 +191,21 @@ class DesaPengaduanList extends Component {
                             cancelButtonText: "Batal",
                           }).then((result) => {
                             if (result.isConfirmed) {
+                              const updatedPengaduanList =
+                                this.state.pengaduanList.map((pengaduan, i) => {
+                                  if (i === index) {
+                                    return {
+                                      ...pengaduan,
+                                      status: "done",
+                                    };
+                                  }
+                                  return pengaduan;
+                                });
+
+                              this.setState({
+                                pengaduanList: updatedPengaduanList,
+                              });
+
                               Swal.fire({
                                 icon: "success",
                                 title: "Pengaduan berhasil diselesaikan",
